39a82d7219877b3e56eabdf8b26b072e2ccb36be
mobabeke <mobabeke@stuent.42wolfsburg.de>
2022-12-15 21:52:23 +0100

    strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mobabeke <mobabeke@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 21:30:09 by mobabeke          #+#    #+#             */
+/*   Updated: 2022/12/15 21:51:30 by mobabeke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (*str1 && i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
+}
+// int main(){
+// 	printf("%d",ft_strncmp("hello", "helLo",4));
+// }
