8455b68c9ac95a2edb7f536ce28c05a8711ba774
mobabeke <mobabeke@student.42wolfsburg.de>
2022-12-28 00:19:17 +0100

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -11,23 +11,31 @@
 /* ************************************************************************** */
 
 #include<stdio.h>
+#include<string.h>
+#include "libft.h"
 
-size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t	size)
 {
+	size_t	dest_len;
+	size_t	src_len;
 	size_t	i;
+	size_t	j;
 
+	src_len = ft_strlen(src);
+	dest_len = ft_strlen(dst);
 	i = 0;
-	while (*dst && i < dstsize)
+	j = dest_len;
+	if (dest_len + 1 < size && size > 0)
 	{
-		dst++;
-		i++;
+		while (src[i] && dest_len + i < size - 1)
+		{
+			dst[j] = src[i];
+			j++;
+			i++;
+		}
+		dst[j] = 0;
 	}
-	while (*src && i < dstsize)
-	{
-		*dst = *src;
-		dst++;
-		src++;
-		i++;
-	}
-	return (i);
+	if (dest_len >= size)
+		dest_len = size;
+	return (dest_len + src_len);
 }
